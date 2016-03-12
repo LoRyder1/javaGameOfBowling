e1da5acca4d5782ba93a2c1f508cfd0f7e32cc1d
Lovinder Pnag <lspnag@gmail.com>
2016-03-11 23:38:27 -0500

    scoreFrame method and use frameIndex

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -4,47 +4,52 @@ public class BowlingGame {
     private int total;
     private int rolls[] = new int[21];
     private int currentRoll = 0;
+    private int frameIndex = 0;
 
     public void roll(int pins) {
         rolls[currentRoll++] += pins;
     }
 
     public int score() {
-        currentRoll = 0;
+//        InterpretFrame frame = new InterpretFrame();
         for(int frame = 0; frame < 10; frame++) {
-            if (isStrike()) {
-                total += 10 + strikeBonus();
-                currentRoll++;
-            }
-            else if (isSpare()) {
-                total += 10 + spareBonus();
-                currentRoll += 2;
-            } else {
-                total += sumAllPinsInFrame();
-                currentRoll += 2;
-            }
+            scoreFrame();
         }
         return total;
     }
 
+    private void scoreFrame() {
+        if (isStrike()) {
+            total += 10 + strikeBonus();
+            frameIndex++;
+        }
+        else if (isSpare()) {
+            total += 10 + spareBonus();
+            frameIndex += 2;
+        } else {
+            total += sumAllPinsInFrame();
+            frameIndex += 2;
+        }
+    }
+
     private int sumAllPinsInFrame() {
-        return rolls[currentRoll] + rolls[currentRoll + 1];
+        return rolls[frameIndex] + rolls[frameIndex + 1];
     }
 
     private int spareBonus() {
-        return rolls[currentRoll + 2];
+        return rolls[frameIndex + 2];
     }
 
     private int strikeBonus() {
-        return rolls[currentRoll + 1] + rolls[currentRoll + 2];
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
     }
 
     private boolean isSpare() {
-        return rolls[currentRoll] + rolls[currentRoll + 1] == 10;
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
     }
 
     private boolean isStrike() {
-        return rolls[currentRoll] == 10;
+        return rolls[frameIndex] == 10;
     }
 
 }
